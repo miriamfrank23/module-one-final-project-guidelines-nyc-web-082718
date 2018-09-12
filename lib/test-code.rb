be089af96b083e3805b0a07110ed262f1210e894
github username <github email address>
2018-09-11 20:51:19 -0400

    added way to extract info from yelp search

--- lib/test-code.rb
+++ lib/test-code.rb
@@ -9,8 +9,9 @@ API_HOST = "https://api.yelp.com"
 SEARCH_PATH = "/v3/businesses/search"
 BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
 
+num = 1
+SEARCH_LIMIT = num
 
-SEARCH_LIMIT = 1
 
 def search(term, location)
   url = "#{API_HOST}#{SEARCH_PATH}"
@@ -19,11 +20,28 @@ def search(term, location)
     location: location,
     limit: SEARCH_LIMIT
   }
-
   response = HTTP.auth("Bearer #{API_KEY}").get(url, params: params)
   response.parse
 end
 
+def run(term, location)
+  search(term, location)
+end
 
+def search_calls(search_result, number)
+  i = 0
+  while i < number
+    name = search_result["businesses"][i]["name"]
+    rating = search_result["businesses"][i]["rating"]
+    zip_code = search_result["businesses"][i]["location"]["zip_code"]
+    image_url = search_result["businesses"][i]["image_url"]
+    type = search_result["businesses"][i]["categories"][0]["alias"]
+    display_address = search_result["businesses"][i]["location"]["display_address"]
+    #its an array of alias. so can call with 1 or 2
+    # binding.pry
+    i += 1
+  end
+end
 
-binding.pry
+search_result = run('dessert', 'New York City')
+search_calls(search_result, num)
